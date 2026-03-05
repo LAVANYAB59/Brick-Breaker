3d65f05f72107d580c53b5b48b4fb92fea4f0b01
Lavanya <boopathilavanya27@gmail.com>
2026-03-05 22:21:00 +0530

    Initial Commit

---
+++ BrickBreaker/src/com/kce/brick/main/GameMain.java
@@ -0,0 +1,24 @@
+package com.kce.brick.main;
+import com.kce.brick.bean.Game;
+import com.kce.brick.service.GameService;
+import java.util.Scanner;
+public class GameMain {
+    public static void main(String[] args) {
+        Game game = new Game();
+        GameService service = new GameService(game);
+        service.initialize();
+        service.printGrid();
+        Scanner sc = new Scanner(System.in);
+        while (game.getLives() > 0) {
+            System.out.println("Enter Command (St / Lt / Rt / Exit): ");
+            String command = sc.next();
+            if (command.equalsIgnoreCase("Exit"))
+                break;
+            service.moveBall(command);
+            service.printGrid();
+            System.out.println("Ball count: " + game.getLives());
+        }
+        System.out.println("Game Over!");
+        sc.close();
+    }
+}
